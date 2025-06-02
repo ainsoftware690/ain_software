55c25b1f2d31965ec99a1d76bfe010fa8663b322
Shahid7424 <shahshahid121212@gmail.com>
2025-06-03 02:16:10 +0530

    fixed error

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,4 +1,3 @@
-// ./src/app/lib/mongodb.ts
 import { MongoClient } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
@@ -11,21 +10,19 @@ const options = {};
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-declare global {
-  // Allow global variable reuse in development
-  // Use `let` to satisfy ESLint rule
-  // Add the globalThis property type properly
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
+// Use module-level globalThis to store the client
+const globalForMongo = globalThis as unknown as {
+  _mongoClientPromise?: Promise<MongoClient>;
+};
 
 if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri!, options);
-    global._mongoClientPromise = client.connect();
+  if (!globalForMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalForMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalForMongo._mongoClientPromise!;
 } else {
-  client = new MongoClient(uri!, options);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
